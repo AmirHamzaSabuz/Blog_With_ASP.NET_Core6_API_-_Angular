fc4ae3cdd14d276bd8526aa6b352301162072448
Amir Hamza <hamza_amir@yahoo.com>
2022-09-03 18:05:06 +0600

    create DbContext class and add migration

---
+++ API/HamzaBlog/HamzaBlog.API/Data/HamzaBlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HamzaBlog.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HamzaBlog.API.Data
+{
+    public class HamzaBlogDbContext : DbContext
+    {
+        public HamzaBlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public DbSet<Post> Posts { get; set; }
+    }
+}
